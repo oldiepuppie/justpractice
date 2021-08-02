55fa7c55c8202092aa535d44a51f671b0774b7fa
이상아 <lebikebihebi@gmail.com>
2021-08-02 21:07:03 +0900

    add states for ui

--- 06-weather-api/src/App.js
+++ 06-weather-api/src/App.js
@@ -1,17 +1,40 @@
+import { useState, useEffect } from 'react';
 import Tester from './Components/Tester';
+import getWeather from './API/getWeather';
 
 function App() {
   document.title = 'Testing Weather API';
+  const [inputCity, setInputCity] = useState('');
+  const [city, setCity] = useState('city');
+  const [temperature, setTemperature] = useState('temp');
+  const [icon, setIcon] = useState('icon');
+  const [description, setDescription] = useState('desc');
+
+  const inputHandler = (event) => {
+    setInputCity(event.target.value);
+  }
+
+  const buttonHandler = () => {
+    // useEffect(() => {
+
+    // }, [])
+    setInputCity('');
+  }
+
   return (
     <div className="App">
       <h1>Testing Weather API</h1>
-      <input type="text" />
-      <button>찾기</button>
-      <Tester 
-        city={'tester'}
-        temperature={'tester'}
-        icon={'tester'}
-        description={'tester'}
+      <input
+        type="text"
+        value={inputCity}
+        onChange={inputHandler}
+      />
+      <button onClick={buttonHandler}>찾기</button>
+      <Tester
+        city={city}
+        temperature={temperature}
+        icon={icon}
+        description={description}
       />
     </div>
   );
