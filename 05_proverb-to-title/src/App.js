5a66be9641497cc76de92b43a2c2bf9c0ecdefc3
이상아 <lebikebihebi@gmail.com>
2021-08-02 19:48:17 +0900

    add new proverbs

--- 05_proverb-to-title/src/App.js
+++ 05_proverb-to-title/src/App.js
@@ -1,13 +1,28 @@
 import { useState } from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  font-size: 1.2rem
+`;
+
+const ProverbBox = styled.p`
+  font-size: 1.5rem;
+  line-height: 160%;
+  padding: .5rem .5rem;
+  background-color: #FDFAF6;
+`;
 
 function App() {
   const proverbs = [
-    "좋은 소프트웨어의 기능이란 복잡한 것을 간단하게 보이도록 만드는 것이다. - Grady Booch -",
-    "손으로 10초면 충분히 할 수 있는 일을 컴퓨터로 하루 종일 프로그래밍해서 자동으로 수행할 때, 나는 더할 나위 없이 큰 행복을 느낀다. - 더글라스 노엘 애덤스 -",
-    "C는 유별나고 결함이 있으며 엄청나게 성공했다. - 데니스 리치 -",
-    "나는 바닷가에서 노는 한 소년과 같았다. 내가 더 예쁜 조개와 더 동그란 조약돌을 찾는 동안 거대한 진리의 바다는 여전히 말없이 내 눈 앞에 펼쳐져 있었다. - 아이작 뉴턴 -",
-    "내일 우리는 더 빨리 달리고 더 멀리 팔을 뻗을 것이다...... 그러면 마침내 어느 찬란한 아침...... 그러므로 우리는 물결을 거스르는 배처럼, 쉴새없이 과거 속으로 밀려나면서도 끝내 앞으로 나아가는 것이다. - 위대한 개츠비, 프란시스 스콧 피츠제럴드 -"
+    '좋은 디자인은 그 때문에 소모되는 비용보다 빠르게 가치가 쌓인다.',
+    '파이썬이 베이식과 동급이라면 옵티머스 프라임은 트럭이다.',
+    '말은 쉽지, 코드를 보여줘.',
+    'C는 유별나고, 결함 있고,  엄청나게 성공했다.',
+    '이론상, 이론과 실제는 같다. 실제로는, 그렇지 않다.',
+    '프로그래밍은 자기 얼굴을 차는 것과 같아서, 조만간 코피가 날 것이다.',
+    '인간적인 반복, 성스러운 재귀'
   ];
+  // https://blog.fupfin.com/?p=11
 
   const [proverb, setProverb] = useState('버튼을 클릭하세요');
   const buttonHandler = () => {
@@ -17,8 +32,8 @@ function App() {
 
   return (
     <div className="App">
-      <h1>{proverb}</h1>
-      <button onClick={buttonHandler}>Reset Title</button>
+      <ProverbBox>{proverb}</ProverbBox>
+      <StyledButton onClick={buttonHandler}>Reset Title</StyledButton>
     </div>
   );
 }
