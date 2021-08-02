d51d663185b268e4e39bab72024b0c5f6ffe8151
이상아 <lebikebihebi@gmail.com>
2021-08-02 20:57:41 +0900

    draft getWeather

--- 06-weather-api/src/API/getWeather.js
+++ 06-weather-api/src/API/getWeather.js
@@ -1,14 +1,26 @@
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
-const getWeather = (cityName) => {
+const getWeather = async (cityName, stateLoadingCallback,stateDataCallback) => {
   const url = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.API_KEY}`;
+  stateLoadingCallback(true);
+  
+  const response = await fetch(url);
+  const data = await response.json();
 
-  useEffect(() => {
-    fetch(url)
-      .then(res => res.json())
-      .then
-  })
+  stateLoadingCallback(false);
+  stateDataCallback(data);
+
+  // if (!cityName) return;
+  // useEffect(() => {
+  //   fetch(url)
+  //     .then(res => res.json())
+  //     .then
+  // })
 }
 
 export default getWeather;
+
+
+// ref.
 // https://ko.reactjs.org/docs/faq-ajax.html#how-can-i-make-an-ajax-call
+// https://github.com/oldiepuppie/apod/blob/master/src/hooks/useGetApod.js
