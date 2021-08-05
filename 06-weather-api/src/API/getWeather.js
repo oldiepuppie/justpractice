4e9c0c2f1a71db1dc21a200d202bd964e6374787
이상아 <lebikebihebi@gmail.com>
2021-08-05 20:01:51 +0900

    finish api test

--- 06-weather-api/src/API/getWeather.js
+++ 06-weather-api/src/API/getWeather.js
@@ -1,13 +1,11 @@
 import apiKey from './api-key';
 
 const getWeather = async (cityName) => {
-  const url = `api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 
   const rawData = await fetch(url);
-  console.log(rawData);
   const jsonData = await rawData.json();
-  console.log(jsonData);
-  // return jsonData;
+  return jsonData;
 }
 
 export default getWeather;
