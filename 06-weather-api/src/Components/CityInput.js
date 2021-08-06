11aff442752be4b163cd7d650807fcf925d0599e
이상아 <lebikebihebi@gmail.com>
2021-08-06 23:29:53 +0900

    draft CityInput

--- 06-weather-api/src/Components/CityInput.js
+++ 06-weather-api/src/Components/CityInput.js
@@ -1,8 +1,20 @@
+import { useState } from 'react';
 
-const cityInput = () => {
+const cityInput = ({ buttonHandler }) => {
+  const [inputCity, setInputCity] = useState('');
+  const inputHandler = (event) => {
+    setInputCity(event.target.value);
+  }
+
   return (
     <div className='cityInput'>
-      
+      <input
+        type="text"
+        placeholder="Search by a city"
+        value={inputCity}
+        onChange={inputHandler}
+      />
+      <button onClick={buttonHandler}>찾기</button>
     </div>
   );
 }
