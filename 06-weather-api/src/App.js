dda8d8f5f00a5367fad97578cb03f3d6a32fcdae
이상아 <lebikebihebi@gmail.com>
2021-08-06 23:49:00 +0900

    lift states of CityInput to App

--- 06-weather-api/src/App.js
+++ 06-weather-api/src/App.js
@@ -5,7 +5,6 @@ import CityInput from './Components/CityInput';
 
 function App() {
   document.title = 'Testing Weather API';
-  const [inputCity, setInputCity] = useState('');
   const [city, setCity] = useState('test');
   const [isCity, setIsCity] = useState(false);
   const [weather, setWeather] = useState('test');
@@ -13,16 +12,11 @@ function App() {
   const [iconSrc, setIconSrc] = useState('test');
   const [description, setDescription] = useState('test');
 
-  const inputHandler = (event) => {
-    setInputCity(event.target.value);
-  }
-
   // TODO 잘못된 도시 이름을 입력했을 때 어떻게 처리할지
-  const buttonHandler = async () => {
-    setInputCity('');
+  const buttonHandler = async (inputCity) => {
     const weatherData = await getWeather(inputCity);
-
     const {name, weather, temperature, icon, description} = weatherData;
+
     if (name) {
       setIsCity(true);
       setCity(name);
@@ -36,19 +30,8 @@ function App() {
   return (
     <div className="App">
       <h1>Testing Weather API</h1>
-
-      {/* TODO input, button을 다른 컴포넌트로 분리 */}
       <div style={{color: 'red'}}>test here</div>
       <CityInput buttonHandler={buttonHandler} />
-
-      <input
-        type="text"
-        placeholder="Search by a city"
-        value={inputCity}
-        onChange={inputHandler}
-      />
-      <button onClick={buttonHandler}>찾기</button>
-
       {isCity ?
         <WeatherContainer
           city={city}
