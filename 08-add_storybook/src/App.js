9eb1d4e0606a0d4f95ead58c89ebe62ebc40088d
이상아 <lebikebihebi@gmail.com>
2021-08-09 10:53:09 +0900

    add former files to test

---
+++ 08-add_storybook/src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import React from 'react';
+import Checkbox from './Components/Checkbox';
+import Select from './Components/Select';
+import PopUp from './Components/PopUp';
+import Input from './Components/Input';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>test props and state</h1>
+      <Parent />
+      <Checkbox />
+      <Select />
+      <PopUp />
+      <Input />
+    </div>
+  );
+}
+
+function Parent() {
+  return (
+    <div className="parent">
+      <h2>props</h2>
+      <Child>리액트를 배우고 있습니다.</Child>
+    </div>
+  );
+}
+
+function Child(props) {
+  return (
+    <div className="child">This is a child : {props.children}</div>
+  );
+}
+
+export default App;
