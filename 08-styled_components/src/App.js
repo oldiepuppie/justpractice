16bb5db0b58e54424f11fb7a85c762c295e7a383
이상아 <lebikebihebi@gmail.com>
2021-08-09 14:48:40 +0900

    add button component with props

--- 08-styled_components/src/App.js
+++ 08-styled_components/src/App.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 const Title = styled.h1`
-  font-size: 1.5em;
+  font-size: 2em;
   text-align: center;
   color: palevioletred;
 `;
@@ -11,10 +11,32 @@ const Wrapper = styled.section`
   background: papayawhip;
 `;
 
+const Button = styled.button`
+  background: ${(props) => (props.primary ? "palevioletred" : "white")};
+  color: ${(props) => (props.primary ? "white" : "palevioletred")};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+const Tomato = styled(Button)`
+  color: tomato;
+  border-color: tomato;
+`;
+
 function App() {
   return (
     <Wrapper>
       <Title>Hello World!</Title>
+      <div>
+        <h2>buttons</h2>
+        <Button>Normal</Button>
+        <Button primary>Primary</Button>
+        <Tomato>Tomato</Tomato>
+      </div>
     </Wrapper>
   );
 }
