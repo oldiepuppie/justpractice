c25b70ff766045b1f317c18bf1b3f1bd464f2d21
이상아 <lebikebihebi@gmail.com>
2021-10-18 03:32:53 +0900

    불필요한 폴더 제거

--- 12-orm-from-zero/README.md
+++
@@ -1,9 +0,0 @@
-# orm-from-zero_proverbs
-## todoList1
-- [ ] api 작성  
-- [ ] 스키마 디자인 다이어그램  
-- [ ] 설명  
-
-## todoList2_project-wise
-- [ ] sequelize - foreign key  
-- [ ] plan api  
--- 12-orm-from-zero/routers/proverbs.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-// const { get, post } = require('../controllers/proverbs');
-
-// TODO get, post 라우팅, 컨트롤러
-// router.get('/', get);
-// router.post('/, post);
-
-module.exports = router;
