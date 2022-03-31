09d60e2a237c8cb70053e51ff5245317643a0aba
이상아 <lebikebihebi@gmail.com>
2022-03-31 23:42:11 +0900

    Add Counter - before make and apply redux middleware

--- 22-redux-vlpt/7-redux-middleware/src/App.js
+++ 22-redux-vlpt/7-redux-middleware/src/App.js
@@ -1,7 +1,9 @@
+import CounterContainer from "./containers/CounterContainers";
 function App() {
   return (
     <div className="App">
       <h1>7-redux-middleware</h1>
+      <CounterContainer />
     </div>
   );
 }
