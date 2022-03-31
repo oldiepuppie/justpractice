d25021cbe2933e8357bca0ed3cdd32e17f0ddb43
이상아 <lebikebihebi@gmail.com>
2022-03-31 23:36:39 +0900

    Apply redux modules to project

--- 22-redux-vlpt/7-redux-middleware/src/index.js
+++ 22-redux-vlpt/7-redux-middleware/src/index.js
@@ -2,8 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./modules/index";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
