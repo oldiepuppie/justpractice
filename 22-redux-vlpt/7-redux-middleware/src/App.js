861cf8f92e35c9f5874733d57785898e6c86cc03
이상아 <lebikebihebi@gmail.com>
2022-04-05 21:00:29 +0900

    Create PostListContainer component

--- 22-redux-vlpt/7-redux-middleware/src/App.js
+++ 22-redux-vlpt/7-redux-middleware/src/App.js
@@ -1,8 +1,11 @@
 import CounterContainer from "./containers/CounterContainers";
+import PostListContainer from "./containers/PostListContainer";
+
 function App() {
   return (
     <div className="App">
       <h1>7-redux-middleware</h1>
+      <PostListContainer />
       <CounterContainer />
     </div>
   );
