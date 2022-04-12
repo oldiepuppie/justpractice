077b8d41df432ea76ef59b72a9a4e90a345eba0a
이상아 <lebikebihebi@gmail.com>
2022-04-13 02:48:49 +0900

    [7-5] Add react-router-dom to create a component reading one particular post

--- 22-redux-vlpt/7-2_redux-thunk-promise/src/pages/PostPage.js
+++ 22-redux-vlpt/7-2_redux-thunk-promise/src/pages/PostPage.js
@@ -1,6 +1,7 @@
 import PostContainer from '../containers/PostContainer';
 
 function PostPage({ match }) {
+  // FIXME
   const { id } = match.params;
 
   return <PostContainer postId={parseInt(id, 10)} />;
