902a1dccc98209f5bda6d82945e753fd484a4d3f
이상아 <lebikebihebi@gmail.com>
2022-04-13 02:50:15 +0900

    [7-5][Fix] Adjust new API of react-router-dom, replacing match.params to get Id

--- 22-redux-vlpt/7-2_redux-thunk-promise/src/pages/PostPage.js
+++ 22-redux-vlpt/7-2_redux-thunk-promise/src/pages/PostPage.js
@@ -1,8 +1,10 @@
 import PostContainer from '../containers/PostContainer';
+import { useParams } from 'react-router-dom';
 
-function PostPage({ match }) {
+function PostPage() {
   // FIXME
-  const { id } = match.params;
+
+  const { id } = useParams();
 
   return <PostContainer postId={parseInt(id, 10)} />;
 }
