79351ac63dc3a1a4fd0cfb6582f8868dc560cf2f
이상아 <lebikebihebi@gmail.com>
2022-04-19 20:53:20 +0900

    [7-8] Replace Mockaroo with local server by json-server

--- 22-redux-vlpt/7-2_redux-thunk-promise/src/api/posts.js
+++ 22-redux-vlpt/7-2_redux-thunk-promise/src/api/posts.js
@@ -1,16 +1,12 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const getPosts = async () => {
-  const res = await axios.get('https://api.mockaroo.com/api/c9780780?count=20&key=ae081390');
-  const { data } = await res;
-// console.log(data);
+  const res = await axios.get("http://localhost:4000/posts");
+  const { data } = res;
   return data;
-}
+};
 
-export const getPostById = async id => {
-  const postList = await getPosts();
-  // console.log(postList.filter(post => post.id === id));
-  return postList.find(post => post.id === id);
-}
-
-// TODO remove console.log
+export const getPostById = async (id) => {
+  const response = await axios.get(`http://localhost:4000/posts/${id}`);
+  return response.data;
+};
