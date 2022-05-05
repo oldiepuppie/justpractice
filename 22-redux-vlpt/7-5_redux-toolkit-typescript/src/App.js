3ca37b14795cba1063eccd0921b8fe7ee701715a
이상아 <lebikebihebi@gmail.com>
2022-05-05 19:43:20 +0900

    Feat: Create new project

---
+++ 22-redux-vlpt/7-5_redux-toolkit-typescript/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>redux-toolkit</h1>
+      <a href="https://redux-toolkit.js.org/tutorials/typescript">
+        <h2>redux toolkit typescript quickstart</h2>
+      </a>
+    </div>
+  );
+}
+
+export default App;
