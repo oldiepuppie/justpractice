b94930bd49e015e8c3329a259bbbfb75202a7319
이상아 <lebikebihebi@gmail.com>
2022-05-05 17:07:18 +0900

    [Chore]

--- 22-redux-vlpt/7-2_redux-thunk-promise/src/api/posts.js
+++ 22-redux-vlpt/7-2_redux-thunk-promise/src/api/posts.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 export const getPosts = async () => {
-  const res = await axios.get("http://localhost:4000/posts");
+  const res = await axios.get("/posts");
   const { data } = res;
   return data;
 };
 
 export const getPostById = async (id) => {
-  const response = await axios.get(`http://localhost:4000/posts/${id}`);
+  const response = await axios.get(`/posts/${id}`);
   return response.data;
 };
